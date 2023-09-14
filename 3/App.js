895717390c7801f8264ebb34e6b4bb5d1d0aff2e
Ahmed Ali <aali@cinnova.com>
2023-09-15 01:10:36 +0500

    end p3

--- 3/App.js
+++ 3/App.js
@@ -10,11 +10,42 @@ const parentSiblings = React.createElement("div", { id: "parent" }, [
         React.createElement("h1", {}, "I am h1 tag"),
         React.createElement("h2", {}, "I am h2 tag"),
     ]),
-]); 
+]);
 
-// jsx
+// jsx JS XML (it is not html, it is html like syntax)
 const jsx = <h1>Hello world</h1>
 
 
+// functional component (capital firstletter)
+// returns some jsx
+
+const HeadingComponent = () => {
+    return (
+        <div>
+            {a}
+            <h1>functional component </h1>
+            <TitleComponent />
+        </div>
+    )
+}
+
+
+const TitleComponent = () => {
+    return (
+        <div>title</div>
+    )
+}
+
+
+
+// part two
+
+// how to put element into react conponent
+
+let a = (<h1>title element</h1>);
+
+// write any js code in this => {}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsx)
+root.render(<HeadingComponent />)
