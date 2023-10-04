610460b20691617b7e83975a4ea93ad85d9756de
Ahmed Ali <aali@cinnova.com>
2023-10-05 03:05:07 +0500

    ch4 - resturant app

--- 4/App.js
+++ 4/App.js
@@ -3,57 +3,66 @@ import ReactDOM from "react-dom/client";
 import foodlogo from "./assests/foodlogo.png";
 import "./index.css";
 const AppLayout = () => {
-    return (
-        <>
-            <Header />
-            <Body />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
 };
 
-const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img src={foodlogo}
-                    height={100}
-                    width={100} />
-            </div>
+// Inline style
+// const style = {
+//     backgroundcolor:'gray'
+// }
 
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={foodlogo} height={100} width={100} />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search"></div>
-            <div className="res-container">
-                <ResturantCard />
-            </div>
-        </div>
-    );
+  return (
+    <div className="body">
+      <div className="search-bar">Search</div>
+      <div className="res-container">
+        <ResturantCard />
+      </div>
+    </div>
+  );
 };
 
 const ResturantCard = () => {
-    return (
-        <div className="res-card">
-            <h3 className="res-name">Broadway Pizza</h3>
-            <img />
-        </div>
-    );
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://images.deliveryhero.io/image/fd-pk/LH/u4tn-listing.jpg?width=400&height=292"
+      />
+      <h3 className="res-name">Broadway Pizza</h3>
+      <h4 className="res-cuisines">Pizza, burgers, biryani</h4>
+      <h4 className="res-start">4.6 stars</h4>
+      <h4 className="res-wait">10 minutes</h4>
+    </div>
+  );
 };
 
-// stopped at 36
+//
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <AppLayout />);
+root.render(<AppLayout />);
